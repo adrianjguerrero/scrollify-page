25b7e1e539d556f163607cef0aa89e4da0b61776
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-05-21 08:59:30 -0400

    webpack notifier

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const OptimizeCssAsset = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require("webpack");
 const autoprefixer = require("autoprefixer");
+var WebpackNotifierPlugin = require('webpack-notifier');
+
 
 module.exports = {
   entry: './js/bundle.js',
@@ -11,18 +13,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  mode: 'production',
   optimization:{
     minimizer:[
       new OptimizeCssAsset(),
       new TerserPlugin()
     ]
   },
+  watch: true,
   module: {
     rules: [{
         test: /\.scss$/, use:[MiniCssExtractPlugin.loader,'css-loader','postcss-loader','sass-loader'],
         }]
     },
     plugins : [new MiniCssExtractPlugin(),
+      new WebpackNotifierPlugin(),
         new webpack.LoaderOptionsPlugin({
           options: {
               postcss: [
