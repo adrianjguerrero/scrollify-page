ca255324d2a5663c75c3c5d5b31618832600ce94
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-05-23 08:54:21 -0400

    estilos y arreglos yea

--- js/slide-phone.js
+++ js/slide-phone.js
@@ -6,7 +6,7 @@ $(function() {
       before: function(){
         $('.secciones').each(function(idx,element){
           if(element == $.scrollify.current()[0]){
-              $('.container-phone .slides').css('transform','translateY(-'+ 480 * (idx + 1)+'px)');
+              $('.container-phone .slides').css('transform','translateY(-'+ 470 * (idx + 1)+'px)');
               // la idea es que avance tanto el alto del contenedor, el alto en este caso es 480
             }
         
